694f0d927ff974ee2749a6b6ade491c29d962f62
Artem Olifirenko <97922268+Artem-Olifirenko@users.noreply.github.com>
2023-11-11 17:12:07 +0400

    Update image_preprocessing.py

--- src/image_preprocessing.py
+++ src/image_preprocessing.py
@@ -14,7 +14,7 @@ def load_image(image_path):
 
 def normalize_image(image, target_size=(256, 256)):
     """
-    Нормализует размер изображения и преобразует его в градации серого.
+    Нормализует размер изображения.
     """
     image = cv2.resize(image, target_size, interpolation=cv2.INTER_AREA)
     return image
@@ -23,36 +23,32 @@ def adjust_brightness_contrast(image, brightness=0, contrast=0):
     """
     Корректирует яркость и контраст изображения.
     """
-    if brightness != 0:
-        if brightness > 0:
-            shadow = brightness
-            highlight = 255
-        else:
-            shadow = 0
-            highlight = 255 + brightness
-        alpha_b = (highlight - shadow) / 255
-        gamma_b = shadow
+    # ... (предыдущий код коррекции яркости и контраста)
 
-        buf = cv2.addWeighted(image, alpha_b, image, 0, gamma_b)
-    else:
-        buf = image.copy()
+def correct_color_balance(image, alpha=1.0, beta=1.0, gamma=0.0):
+    """
+    Корректирует цветовой баланс изображения.
+    """
+    image = cv2.convertScaleAbs(image, alpha=alpha, beta=beta)
+    image = cv2.addWeighted(image, alpha, np.zeros_like(image), 0, gamma)
+    return image
 
-    if contrast != 0:
-        f = 131 * (contrast + 127) / (127 * (131 - contrast))
-        alpha_c = f
-        gamma_c = 127 * (1 - f)
-
-        buf = cv2.addWeighted(buf, alpha_c, buf, 0, gamma_c)
-
-    return buf
+def reduce_noise(image):
+    """
+    Уменьшает шум на изображении.
+    """
+    return cv2.GaussianBlur(image, (5, 5), 0)
 
 # Пример использования
 image_path = 'path/to/your/image.jpg'
 image = load_image(image_path)
 normalized_image = normalize_image(image)
 adjusted_image = adjust_brightness_contrast(normalized_image, brightness=30, contrast=30)
+color_corrected_image = correct_color_balance(adjusted_image, alpha=1.2, beta=1.2)
+noise_reduced_image = reduce_noise(color_corrected_image)
 
 # Показать изображение
-cv2.imshow('Processed Image', adjusted_image)
+cv2.imshow('Processed Image', noise_reduced_image)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
+
