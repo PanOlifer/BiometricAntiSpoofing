f407abe1bde1f7c940d016604d2ba14b78db99bf
Владимир Москвин <dzhankui@users.noreply.github.com>
2023-11-20 17:08:25 +0300

    Fixed square moves

--- src/Square.py
+++ src/Square.py
@@ -1,71 +1,81 @@
-"""
-installation pygame doesn't work when compiler is anaconda..? Try python 3.11
-"""
 import pygame
 
+# Initialize Pygame
 pygame.init()
 
-screen_info = pygame.display.Info()
-screen_width = screen_info.current_w
-screen_height = screen_info.current_h
+# Create the window surface
+window_surface = pygame.display.set_mode((800, 600))  # Set your desired window dimensions
+pygame.display.set_caption("Green Square")
 
-window_width = screen_width // 2
-window_height = screen_height // 2
-
-window_surface = pygame.display.set_mode((window_width, window_height), pygame.RESIZABLE)
-pygame.display.set_caption("Green Fuckin' Square")
-
+# Set the initial position of the square
 square_size = 50
 square_x, square_y = 0, 0
 
+# Set the green color (R, G, B)
 green = (0, 255, 0)
 
+# Set the maximum number of pixels to move in each frame
 max_move_pixels = 2
-# TODO Fix windows size to run it in fullscreen mode
-# TODO Fix square movements
+
+# Set the movement phases
 phases = [
-    ["right", window_width - square_size, 0],    # Move to the right upper corner
-    ["down", window_width - square_size, window_height - square_size],  # Move to the left lower corner
-    ["left", 0, window_height - square_size],    # Move to the upper left corner
-    ["up", 0, 0]                                # Move back to the left upper corner
+    ["right", True],   # Move to the right window border
+    ["down", False],   # Move to the bottom window border
+    ["left", True],    # Move to the left window border
+    ["up", False]      # Move to the top window border
 ]
 
+# Set the initial phase index
 current_phase = 0
 
+# Get the window dimensions
+window_width, window_height = pygame.display.get_surface().get_size()
+
+# Main game loop
 running = True
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-        elif event.type == pygame.VIDEORESIZE:
-            # Resize the window
-            window_width = event.w
-            window_height = event.h
-            window_surface = pygame.display.set_mode((window_width, window_height), pygame.RESIZABLE)
 
-    phase_direction, target_x, target_y = phases[current_phase]
+    # Get the current phase details
+    phase_direction, is_horizontal = phases[current_phase]
 
-    move_x = max_move_pixels if target_x > square_x else -max_move_pixels
-    move_y = max_move_pixels if target_y > square_y else -max_move_pixels
+    # Update the square position based on the current phase
+    if phase_direction == "right":
+        square_x += max_move_pixels
+        if square_x >= window_width - square_size:
+            square_x = window_width - square_size
+            current_phase += 1
+    elif phase_direction == "down":
+        square_y += max_move_pixels
+        if square_y >= window_height - square_size:
+            square_y = window_height - square_size
+            current_phase += 1
+    elif phase_direction == "left":
+        square_x -= max_move_pixels
+        if square_x <= 0:
+            square_x = 0
+            current_phase += 1
+    elif phase_direction == "up":
+        square_y -= max_move_pixels
+        if square_y <= 0:
+            square_y = 0
+            current_phase += 1
 
-    square_x += move_x
-    square_y += move_y
+    # Wrap around the phases if reaching the end
+    current_phase %= len(phases)
 
-    if (move_x > 0 and square_x >= target_x) or (move_x < 0 and square_x <= target_x):
-        square_x = target_x
-    if (move_y > 0 and square_y >= target_y) or (move_y < 0 and square_y <= target_y):
-        square_y = target_y
-
-        current_phase += 1
-        current_phase %= len(phases)  # Wrap around if reaching the end of the phases
-
+    # Fill the window with a white background
     window_surface.fill((255, 255, 255))
 
+    # Draw the green square
     pygame.draw.rect(window_surface, green, (square_x, square_y, square_size, square_size))
 
+    # Update the display
     pygame.display.flip()
+    # Delay for a short time
+    pygame.time.delay(5)
 
-    pygame.time.delay(50)
-
-
+# Quit Pygame
 pygame.quit()
