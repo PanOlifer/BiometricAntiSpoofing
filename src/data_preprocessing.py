2001134e8e21c8a5723148e6a48c9f39bb586a4f
Artem Olifirenko <97922268+Artem-Olifirenko@users.noreply.github.com>
2023-11-22 22:01:38 +0400

    Update data_preprocessing.py

--- src/data_preprocessing.py
+++ src/data_preprocessing.py
@@ -5,48 +5,47 @@ from tensorflow.keras.preprocessing.image import ImageDataGenerator
 import nbformat
 
 def extract_data_from_notebook(notebook_path):
-    """
-    Извлекает данные из Jupyter ноутбука.
-    """
-    with open(notebook_path, 'r', encoding='utf-8') as file:
-        nb = nbformat.read(file, as_version=4)
+    try:
+        with open(notebook_path, 'r', encoding='utf-8') as file:
+            nb = nbformat.read(file, as_version=4)
 
-    # Пример кода для извлечения данных из ноутбука
-    # Это нужно доработать в зависимости от структуры и содержания ноутбука
-    data = None
-    for cell in nb.cells:
-        if cell.cell_type == 'code':
-            # Пример: поиск и извлечение определенных данных из кодовых ячеек
-            # ...
-            pass
+        data = {}
+        for cell in nb.cells:
+            if cell.cell_type == 'code':
+                if 'extracted_data' in cell['source']:
+                    exec(cell['source'])
+                    data['extracted_data'] = extracted_data
+
+    except Exception as e:
+        print(f"Ошибка при извлечении данных из ноутбука: {e}")
+        data = None
 
     return data
 
 def preprocess_image(image, target_size=(224, 224)):
-    """
-    Преобразует изображение: изменяет размер и нормализует.
-    """
     image = cv2.resize(image, target_size)
-    image = image / 255.0  # Нормализация
+    image = image / 255.0
     return image
 
 def prepare_data_generator(data, batch_size=32):
-    """
-    Подготавливает генератор данных для модели.
-    """
-    datagen = ImageDataGenerator(rescale=1./255)
-    generator = datagen.flow_from_dataframe(
-        dataframe=data,
-        x_col='file_path',  # Название столбца с путями к файлам
-        y_col='label',      # Метки
-        class_mode='raw',   # Тип меток
-        batch_size=batch_size,
-        shuffle=True,       # Перемешивание данных
-        target_size=(224, 224))  # Размер изображений
+    try:
+        datagen = ImageDataGenerator(rescale=1./255)
+        generator = datagen.flow_from_dataframe(
+            dataframe=data,
+            x_col='file_path',
+            y_col='label',
+            class_mode='raw',
+            batch_size=batch_size,
+            shuffle=True,
+            target_size=(224, 224))
+    except Exception as e:
+        print(f"Ошибка при создании генератора данных: {e}")
+        generator = None
+
     return generator
 
 # Пример использования
 notebook_path = 'notebooks/webcam_pics_getting.ipynb'
 data = extract_data_from_notebook(notebook_path)
-
-data_generator = prepare_data_generator(data)
+if data is not None:
+    data_generator = prepare_data_generator(data)
